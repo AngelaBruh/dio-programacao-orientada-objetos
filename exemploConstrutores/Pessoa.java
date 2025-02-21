cb06143990b27d2207c0937cc1172461cd9a0325
Bruna Angela <150439932+AngelaBruh@users.noreply.github.com>
2025-02-21 14:33:00 -0300

    Consegui recuperar os arquivos
    Consegui recuperar os arquivos do commit excluido, porém não conseguir usar o Git para move-los, então arrastei as pastas aqui dentro.

---
+++ exemploConstrutores/Pessoa.java
@@ -0,0 +1,31 @@
+package exemploConstrutores;
+
+public class Pessoa {
+	private String nome;
+	private String cpf;
+	private String endereco;
+	
+	// Com o botão direito do mouse posso criar automaticamente os getters e setters
+
+	// método construtor
+	// o nome deverá ser igual ao nome da classe
+	public Pessoa (String cpf, String nome) {
+		this.cpf = cpf;
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	//...
+	//setters de nome e cpf ?
+}
